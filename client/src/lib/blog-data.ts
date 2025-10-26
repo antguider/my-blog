bb7ea7bf8dadf1fefb4c4e41699ace8e085f101f
Muthukumar Jayamurugan <muthukcse@gmail.com>
2025-10-26 13:43:30 +0530

    cleanup

--- client/src/lib/blog-data.ts
+++
@@ -1,5 +0,0 @@
-export const blogPostsData = [
-  // This file serves as a reference for the blog post structure
-  // The actual data is stored in the backend storage and served via API
-  // See server/storage.ts for the complete blog posts data
-];
